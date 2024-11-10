44c287cd8244c245715db5d74e9361741fb079d8
javeria <javeria2005pk@gmail.com>
2024-11-10 17:34:29 +0500

    adding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='mt-16 py-6 text-center bg-accent'>
-      @ Tariq | All rights reserved 2024.
+      @ Javeria | All rights reserved 2024.
     </div>
   )
 }
